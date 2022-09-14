943f9c9ed8567f510215276e439d59d59db5a067
icm213 <marekpownuk@gmail.com>
2022-09-14 18:43:27 +0200

    added fading animation to section when toggled

--- src/Section/Section.jsx
+++ src/Section/Section.jsx
@@ -1,18 +1,24 @@
+import { useState, useEffect } from "react"
 import { Home } from "./Home"
 import { Work } from "./Work"
 import { ContactMe } from "./ContactMe"
 
 export const Section = ({triangles}) => {
 
-const hideAndDispaly = (elem) => {
-        return elem
-}
+    const [sect, setSect] = useState([true, false, false, false])
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSect(triangles)
+        }, 300);
+    }, [triangles])
 
     return (
         <section className="section">
-                {triangles[0] ? hideAndDispaly(<Home triangles={triangles} />) : null}
-                {triangles[1] ? hideAndDispaly(<Work triangles={triangles} />) : null}
-                {triangles[2] ? hideAndDispaly(<ContactMe triangles={triangles} />) : null}
+                {sect[0] ? <Home triangles={triangles} /> : null}
+                {sect[1] ? <Work triangles={triangles} /> : null}
+                {sect[2] ? <ContactMe triangles={triangles} /> : null}
         </section>
     )
 }
